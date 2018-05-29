dff397b27cc5256884e7f8b4b1b47823c615585b
Daniel <derelanadecube@gmail.com>
2018-05-29 16:32:29 +0300

    Changes to be committed: 	modified:   configure.ac 	deleted:    contrib/dash-qt.pro 	deleted:    contrib/dashd.bash-completion 	deleted:    contrib/debian/dash-qt.desktop 	deleted:    contrib/debian/dash-qt.install 	deleted:    contrib/debian/dash-qt.lintian-overrides 	deleted:    contrib/debian/dash-qt.protocol 	deleted:    contrib/debian/dash-tx.install 	deleted:    contrib/debian/dashd.bash-completion 	deleted:    contrib/debian/dashd.examples 	deleted:    contrib/debian/dashd.install 	deleted:    contrib/debian/dashd.lintian-overrides 	deleted:    contrib/debian/dashd.manpages 	modified:   contrib/gitian-descriptors/gitian-linux.yml 	modified:   contrib/gitian-descriptors/gitian-osx.yml 	modified:   contrib/gitian-descriptors/gitian-rpi2.yml 	modified:   contrib/gitian-descriptors/gitian-win.yml 	deleted:    contrib/init/dashd.init 	deleted:    contrib/init/dashd.openrc 	deleted:    contrib/init/dashd.openrcconf 	deleted:    contrib/init/dashd.service 	deleted:    contrib/init/org.dash.dashd.plist 	modified:   contrib/linearize/linearize-hashes.py 	modified:   contrib/qos/tc.sh 	modified:   contrib/seeds/makeseeds.py 	modified:   contrib/spendfrom/spendfrom.py 	deleted:    share/pixmaps/dash64.png 	deleted:    share/pixmaps/dash64.xpm 	modified:   src/chainparams.cpp 	modified:   src/chainparamsbase.cpp 	modified:   src/chainparamsseeds.h 	modified:   src/clientversion.h 	deleted:    src/dash-cli-res.rc 	deleted:    src/dash-tx-res.rc 	deleted:    src/dashd-res.rc 	deleted:    src/qt/dash.qrc 	deleted:    src/qt/dash_locale.qrc 	deleted:    src/qt/locale/dash_bg.ts 	deleted:    src/qt/locale/dash_de.ts 	deleted:    src/qt/locale/dash_en.ts 	deleted:    src/qt/locale/dash_es.ts 	deleted:    src/qt/locale/dash_fi.ts 	deleted:    src/qt/locale/dash_fr.ts 	deleted:    src/qt/locale/dash_it.ts 	deleted:    src/qt/locale/dash_ja.ts 	deleted:    src/qt/locale/dash_pt.ts 	deleted:    src/qt/locale/dash_ru.ts 	deleted:    src/qt/locale/dash_sk.ts 	deleted:    src/qt/locale/dash_sv.ts 	deleted:    src/qt/locale/dash_vi.ts 	deleted:    src/qt/locale/dash_zh_CN.ts 	deleted:    src/qt/locale/dash_zh_TW.ts 	deleted:    src/qt/res/dash-qt-res.rc 	deleted:    src/qt/res/images/light/dash_logo_horizontal.png 	deleted:    src/qt/res/images/trad/dash_logo_horizontal.png 	modified:   src/test/netbase_tests.cpp
     Untracked files:
    	contrib/cryptorgcoin-qt.pro
    	contrib/cryptorgcoind.bash-completion
    	contrib/debian/cryptorgcoin-qt.desktop
    	contrib/debian/cryptorgcoin-qt.install
    	contrib/debian/cryptorgcoin-qt.lintian-overrides
    	contrib/debian/cryptorgcoin-qt.protocol
    	contrib/debian/cryptorgcoin-tx.install
    	contrib/debian/cryptorgcoind.bash-completion
    	contrib/debian/cryptorgcoind.examples
    	contrib/debian/cryptorgcoind.install
    	contrib/debian/cryptorgcoind.lintian-overrides
    	contrib/debian/cryptorgcoind.manpages
    	contrib/init/cryptorgcoind.init
    	contrib/init/cryptorgcoind.openrc
    	contrib/init/cryptorgcoind.openrcconf
    	contrib/init/cryptorgcoind.service
    	contrib/init/org.cryptorgcoin.cryptorgcoind.plist
    	keys/
    	share/pixmaps/cryptorgcoin64.png
    	share/pixmaps/cryptorgcoin64.xpm
    	src/cryptorgcoin-cli-res.rc
    	src/cryptorgcoin-tx-res.rc
    	src/cryptorgcoind-res.rc
    	src/qt/cryptorgcoin.qrc
    	src/qt/cryptorgcoin_locale.qrc
    	src/qt/locale/cryptorgcoin_bg.ts
    	src/qt/locale/cryptorgcoin_de.ts
    	src/qt/locale/cryptorgcoin_en.ts
    	src/qt/locale/cryptorgcoin_es.ts
    	src/qt/locale/cryptorgcoin_fi.ts
    	src/qt/locale/cryptorgcoin_fr.ts
    	src/qt/locale/cryptorgcoin_it.ts
    	src/qt/locale/cryptorgcoin_ja.ts
    	src/qt/locale/cryptorgcoin_pt.ts
    	src/qt/locale/cryptorgcoin_ru.ts
    	src/qt/locale/cryptorgcoin_sk.ts
    	src/qt/locale/cryptorgcoin_sv.ts
    	src/qt/locale/cryptorgcoin_vi.ts
    	src/qt/locale/cryptorgcoin_zh_CN.ts
    	src/qt/locale/cryptorgcoin_zh_TW.ts
    	src/qt/res/cryptorgcoin-qt-res.rc
    	src/qt/res/images/light/cryptorgcoin_logo_horizontal.png
    	src/qt/res/images/trad/cryptorgcoin_logo_horizontal.png

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -51,8 +51,8 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "Wired 09/Jan/2014 The Grand Experiment Goes Live: Overstock.com Is Now Accepting Bitcoins";
-    const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+    const char* pszTimestamp = "Dash fork for cryptorg 29.05.2018";
+    const CScript genesisOutputScript = CScript() << ParseHex("04f490d20c37edd7abd4690631b788965144bef914f9fd1b3ab1f91c83673baffbdc8d0f3331e0bcd1916d67bd955e2152990b3fb09b8ef82e81338b0701db4931") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
@@ -127,26 +127,27 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xbf;
-        pchMessageStart[1] = 0x0c;
-        pchMessageStart[2] = 0x6b;
-        pchMessageStart[3] = 0xbd;
-        vAlertPubKey = ParseHex("048240a8748a80a286b270ba126705ced4f2ce5a7847b3610ea3c06513150dade2a8512ed5ea86320824683fc0818f0ac019214973e677acd1244f6d0571fc5103");
-        nDefaultPort = 9999;
+        pchMessageStart[0] = 0x1c;
+        pchMessageStart[1] = 0xbd;
+        pchMessageStart[2] = 0xcb;
+        pchMessageStart[3] = 0x4f;
+        vAlertPubKey = ParseHex("04af1b783f142314a003879834e91ebace28eb0d46cda3701f87cdbfb83ec1a67ac02a6d6427176c9cff21b1874301fe4a41b6e5841f81e4d6c84ac68489e3ed06");
+        nDefaultPort = 9887;
         nMaxTipAge = 6 * 60 * 60; // ~144 blocks behind -> 2 x fork detection time, was 24 * 60 * 60 in bitcoin
         nDelayGetHeadersTime = 24 * 60 * 60;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1390095618, 28917698, 0x1e0ffff0, 1, 50 * COIN);
+        //time, nNonce, nBits
+        genesis = CreateGenesisBlock(1527638400, 8394436, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000ef5996cd119009a425709f6fb04a3a36cdaa44183d4d03045b25d71aaed"));
+        assert(genesis.hashMerkleRoot == uint256S("0x2fd530d1f9b4c9ea52a617b5630dde34a46533a62f4786027b213745b94e81da"));
 
-
-        vSeeds.push_back(CDNSSeedData("dash.org", "dnsseed.dash.org"));
-        vSeeds.push_back(CDNSSeedData("dashdot.io", "dnsseed.dashdot.io"));
-        vSeeds.push_back(CDNSSeedData("masternode.io", "dnsseed.masternode.io"));
-        vSeeds.push_back(CDNSSeedData("dashpay.io", "dnsseed.dashpay.io"));
+        vSeeds.clear();
+        //vSeeds.push_back(CDNSSeedData("dash.org", "dnsseed.dash.org"));
+        //vSeeds.push_back(CDNSSeedData("dashdot.io", "dnsseed.dashdot.io"));
+        //vSeeds.push_back(CDNSSeedData("masternode.io", "dnsseed.masternode.io"));
+        //vSeeds.push_back(CDNSSeedData("dashpay.io", "dnsseed.dashpay.io"));
 
         // Dash addresses start with 'X'
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,76);
@@ -172,35 +173,15 @@ public:
 
         nPoolMaxTransactions = 3;
         nFulfilledRequestExpireTime = 60*60; // fulfilled requests expire in 1 hour
-        strSporkPubKey = "04549ac134f694c0243f503e8c8a9a986f5de6610049c40b07816809b0d1d06a21b07be27b9bb555931773f62ba6cf35a25fd52f694d4e1106ccd237a7bb899fdd";
+        strSporkPubKey = "04221a7ef407e2d194db63ffa36d9a209b9a90a03a33415198688b0685180f16adcec083565a208b1deb1254c7f71e5743c19bf87d9a9ac968a797cdd7585fe6d6";
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            (  1500, uint256S("0x000000aaf0300f59f49bc3e970bad15c11f961fe2347accffff19d96ec9778e3"))
-            (  4991, uint256S("0x000000003b01809551952460744d5dbb8fcbd6cbae3c220267bf7fa43f837367"))
-            (  9918, uint256S("0x00000000213e229f332c0ffbe34defdaa9e74de87f2d8d1f01af8d121c3c170b"))
-            ( 16912, uint256S("0x00000000075c0d10371d55a60634da70f197548dbbfa4123e12abfcbc5738af9"))
-            ( 23912, uint256S("0x0000000000335eac6703f3b1732ec8b2f89c3ba3a7889e5767b090556bb9a276"))
-            ( 35457, uint256S("0x0000000000b0ae211be59b048df14820475ad0dd53b9ff83b010f71a77342d9f"))
-            ( 45479, uint256S("0x000000000063d411655d590590e16960f15ceea4257122ac430c6fbe39fbf02d"))
-            ( 55895, uint256S("0x0000000000ae4c53a43639a4ca027282f69da9c67ba951768a20415b6439a2d7"))
-            ( 68899, uint256S("0x0000000000194ab4d3d9eeb1f2f792f21bb39ff767cb547fe977640f969d77b7"))
-            ( 74619, uint256S("0x000000000011d28f38f05d01650a502cc3f4d0e793fbc26e2a2ca71f07dc3842"))
-            ( 75095, uint256S("0x0000000000193d12f6ad352a9996ee58ef8bdc4946818a5fec5ce99c11b87f0d"))
-            ( 88805, uint256S("0x00000000001392f1652e9bf45cd8bc79dc60fe935277cd11538565b4a94fa85f"))
-            ( 107996, uint256S("0x00000000000a23840ac16115407488267aa3da2b9bc843e301185b7d17e4dc40"))
-            ( 137993, uint256S("0x00000000000cf69ce152b1bffdeddc59188d7a80879210d6e5c9503011929c3c"))
-            ( 167996, uint256S("0x000000000009486020a80f7f2cc065342b0c2fb59af5e090cd813dba68ab0fed"))
-            ( 207992, uint256S("0x00000000000d85c22be098f74576ef00b7aa00c05777e966aff68a270f1e01a5"))
-            ( 312645, uint256S("0x0000000000059dcb71ad35a9e40526c44e7aae6c99169a9e7017b7d84b1c2daf"))
-            ( 407452, uint256S("0x000000000003c6a87e73623b9d70af7cd908ae22fee466063e4ffc20be1d2dbc"))
-            ( 523412, uint256S("0x000000000000e54f036576a10597e0e42cc22a5159ce572f999c33975e121d4d"))
-            ( 523930, uint256S("0x0000000000000bccdb11c2b1cfb0ecab452abf267d89b7f46eaf2d54ce6e652c"))
-            ( 750000, uint256S("0x00000000000000b4181bbbdddbae464ce11fede5d0292fb63fdede1e7c8ab21c")),
-            1507424630, // * UNIX timestamp of last checkpoint block
-            3701128,    // * total number of transactions between genesis and last checkpoint
+            (  0, uint256S("0x00000ef5996cd119009a425709f6fb04a3a36cdaa44183d4d03045b25d71aaed")),
+            1527638400, // * UNIX timestamp of last checkpoint block
+            0,    // * total number of transactions between genesis and last checkpoint
                         //   (the tx=... number in the SetBestChain debug.log lines)
-            5000        // * estimated number of transactions per day after checkpoint
+            2800        // * estimated number of transactions per day after checkpoint
         };
     }
 };
@@ -267,21 +248,21 @@ public:
         pchMessageStart[1] = 0xe2;
         pchMessageStart[2] = 0xca;
         pchMessageStart[3] = 0xff;
-        vAlertPubKey = ParseHex("04517d8a699cb43d3938d7b24faaff7cda448ca4ea267723ba614784de661949bf632d6304316b244646dea079735b9a6fc4af804efb4752075b9fe2245e14e412");
-        nDefaultPort = 19999;
+        vAlertPubKey = ParseHex("041e9035157eec1903f0ae55053ce2ba5bfe3f3e24b4e758499122a2bc048fbf1d9ad9a9cbb8a8bcc38a40cb08545eeeef0a6ebfe3aa2fb178713e5363474a8c8d");
+        nDefaultPort = 19887;
         nMaxTipAge = 0x7fffffff; // allow mining on top of old blocks for testnet
         nDelayGetHeadersTime = 24 * 60 * 60;
         nPruneAfterHeight = 1000;
 
         genesis = CreateGenesisBlock(1390666206UL, 3861367235UL, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000f178cd25eba5c3aab024e24ab97367c1b90540539e90425a803d227ba21"));
+        assert(genesis.hashMerkleRoot == uint256S("0x2fd530d1f9b4c9ea52a617b5630dde34a46533a62f4786027b213745b94e81da"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("dashdot.io",  "testnet-seed.dashdot.io"));
-        vSeeds.push_back(CDNSSeedData("masternode.io", "test.dnsseed.masternode.io"));
+        //vSeeds.push_back(CDNSSeedData("dashdot.io",  "testnet-seed.dashdot.io"));
+        //vSeeds.push_back(CDNSSeedData("masternode.io", "test.dnsseed.masternode.io"));
 
         // Testnet Dash addresses start with 'y'
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,140);
@@ -307,15 +288,13 @@ public:
 
         nPoolMaxTransactions = 3;
         nFulfilledRequestExpireTime = 5*60; // fulfilled requests expire in 5 minutes
-        strSporkPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
+        strSporkPubKey = "0430a1f78e2390c4fef2e9c8cf9e04fd86cf026736b47235ceacde4ef955c2b236da5e2207307954821fef536b9f3f771c2c1be29ab0a06e77774e20e868d7ae39";
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            (    261, uint256S("0x00000c26026d0815a7e2ce4fa270775f61403c040647ff2c3091f99e894a4618"))
-            (   1999, uint256S("0x00000052e538d27fa53693efe6fb6892a0c1d26c0235f599171c48a3cce553b1"))
-            (   2999, uint256S("0x0000024bc3f4f4cb30d29827c13d921ad77d2c6072e586c7f60d83c2722cdcc5")),
+            (    0, uint256S("0x00000f178cd25eba5c3aab024e24ab97367c1b90540539e90425a803d227ba21")),
 
-            1462856598, // * UNIX timestamp of last checkpoint block
+            1527638401, // * UNIX timestamp of last checkpoint block
             3094,       // * total number of transactions between genesis and last checkpoint
                         //   (the tx=... number in the SetBestChain debug.log lines)
             500         // * estimated number of transactions per day after checkpoint
@@ -382,13 +361,13 @@ public:
         pchMessageStart[3] = 0xdc;
         nMaxTipAge = 6 * 60 * 60; // ~144 blocks behind -> 2 x fork detection time, was 24 * 60 * 60 in bitcoin
         nDelayGetHeadersTime = 0; // never delay GETHEADERS in regtests
-        nDefaultPort = 19994;
+        nDefaultPort = 19883;
         nPruneAfterHeight = 1000;
 
         genesis = CreateGenesisBlock(1417713337, 1096447, 0x207fffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
+        assert(consensus.hashGenesisBlock == uint256S("0x4ef3ce0be82c6c09033bc33a72ecadb146317ce64e3ccd84be792c42436c5793"));
+        assert(genesis.hashMerkleRoot == uint256S("0x2fd530d1f9b4c9ea52a617b5630dde34a46533a62f4786027b213745b94e81da"));
 
         vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();  //! Regtest mode doesn't have any DNS seeds.
@@ -403,7 +382,7 @@ public:
 
         checkpointData = (CCheckpointData){
             boost::assign::map_list_of
-            ( 0, uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e")),
+            ( 0, uint256S("0x4ef3ce0be82c6c09033bc33a72ecadb146317ce64e3ccd84be792c42436c5793")),
             0,
             0,
             0
